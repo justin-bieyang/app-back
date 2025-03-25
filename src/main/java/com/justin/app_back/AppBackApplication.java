a27b07b6acb3f4b4124fbe7e69e54dcfb2410816
justin-bieyang <ducaoyang1234567@qq.com>
2025-03-25 20:09:42 +0800

    新增跨域配置

--- src/main/java/com/justin/app_back/AppBackApplication.java
+++ src/main/java/com/justin/app_back/AppBackApplication.java
@@ -1,30 +1,15 @@
 package com.justin.app_back;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
-import com.justin.app_back.intercept.LoginIntercept;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("com.justin.app_back.mapper")
 @EnableKnife4j
 public class AppBackApplication implements WebMvcConfigurer {
-
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginIntercept())
-//                .addPathPatterns("/**") // 拦截路径
-//                .excludePathPatterns("/login")
-//                .excludePathPatterns("/register")
-//                .excludePathPatterns("/**/*.jpg")
-//                .excludePathPatterns("/**/*.png")
-//                .excludePathPatterns("/**/*.gif")
-//                .excludePathPatterns("/**/*.jpeg")
-//                .excludePathPatterns("/**/*.apk");
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppBackApplication.class, args);
--- src/main/java/com/justin/app_back/intercept/LoginIntercept.java
+++ src/main/java/com/justin/app_back/intercept/LoginIntercept.java
@@ -1,18 +1,16 @@
 package com.justin.app_back.intercept;
 
-import com.alibaba.druid.support.json.JSONUtils;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.pagehelper.util.StringUtil;
+
 import com.justin.app_back.utils.JwtUtil;
 import com.justin.app_back.vo.ResultVo;
+import org.springframework.http.HttpMethod;
 import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
@@ -30,8 +28,12 @@ public class LoginIntercept implements HandlerInterceptor {
                              HttpServletResponse response,
                              Object handler) throws Exception {
 
-        // 检测前端发过来的token
+        // 如果是OPTIONS请求，直接放行
+        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
+            return true;
+        }
 
+        // 检测前端发过来的token
         response.setContentType("application/json;charset=utf-8");
         String token = request.getHeader("token");
         if (!StringUtils.hasText(token)) {
